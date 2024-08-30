3841a6a9036ce8278a67771cf6f8f5d21c217379
Vitalis Ian <viterlis254@gmail.com>
2024-08-30 12:20:00 +0300

    initial

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,25 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    """Prints a countdown from 10 to 1, then prints 'Happy New Year!'."""
+    for i in range(10, 0, -1):
+        print(i)
+    print("Happy New Year!")
+
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    """Returns a list of squared integers."""
+    return [x ** 2 for x in int_list]
+
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    """Prints numbers from 1 to 100 with specific rules for multiples of 3 and 5."""
+    for num in range(1, 101):
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
